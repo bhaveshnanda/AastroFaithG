56ba03fc5f58078f7d08dc319ec38417a2075fa4
Bhavesh Nanda <nandabhavesh2003@gmail.com>
2025-08-29 23:54:35 +0530

    Working on Appointment

--- src/pages/Appointment/index.js
+++ src/pages/Appointment/index.js
@@ -14,8 +14,13 @@ export default function BookAppointment() {
     const formData = {
       name: e.target.name.value,
       email: e.target.email.value,
-      date: e.target.date.value,
+      gender: e.target.gender.value,
+      dateofBirth: e.target.date.value,
+      timeOfBirth: e.target.timeOfBirth.value,
+      birthLocation: e.target.birthLocation.value,
       message: e.target.message.value,
+      birthCountry: e.target.birthCountry.value,
+      preferredLanguage: e.target.preferredLanguage.value,
     };
 
     try {
@@ -52,6 +57,15 @@ export default function BookAppointment() {
           <label>Email:</label>
           <input name="email" type="email" placeholder="Your Email" required />
 
+          <label>Gender:</label>
+          <select name="gender" required>
+            <option value="">-- Select Gender --</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+            <option value="prefer_not_say">Prefer not to say</option>
+          </select>
+
           <label>Date of Birth:</label>
           <input name="date" type="date" required />
 
@@ -59,10 +73,20 @@ export default function BookAppointment() {
           <input name="timeOfBirth" type="time" required />
 
           <label>Birth City/Town and State:</label>
-          <input name="birthLocation" type="text" required />
+          <input name="birthLocation" type="text" placeholder="Your City/Town and State" required />
 
-          <label>Message:</label>
+          <label>Birth Country:</label>
+          <input name="birthCountry" type="text" placeholder="Your birth country" required />
+
+          <label>Preferred Language:</label>
+          <input name="preferredLanguage" type="text" placeholder="Your preferred country" required />
+
+          <label>Explain your query:</label>
           <textarea name="message" placeholder="Your Query"></textarea>
+
+          <div className={styles.note}>
+            <p>These details are taken for astrologer's research and horoscope verification</p>
+          </div>
 
           <button type="submit" disabled={loading}>
             {loading ? "Submitting..." : "Submit"}
